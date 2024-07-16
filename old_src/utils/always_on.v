3f8b864c118e5723fc6ef65cb63af6dd058234bf
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-07-16 20:14:13 -0300

    [source] remove old-source

--- old_src/utils/always_on.v
+++
@@ -1,8 +0,0 @@
-//! Always on returns a positive signal, independent of the given entry.
-//!
-//! There is a property that says: A + A' = 0
-//! Since, 0 or 1 is always 1.
-module always_on(output on, input a);
-	not (a_not, a);
-	or  (on, a, a_not);
-endmodule
--- old_src/utils/pipe.v
+++
@@ -1,6 +0,0 @@
-//! Pipe just assigns a signal to another one.
-//!
-//! There is a property that says: A * A = A
-module pipe(output out, input a);
-    and (out, a, a);
-endmodule
