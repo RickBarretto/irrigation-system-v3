366a33a9747b1a3e96f87e469159fc581c8fb15f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-07-16 22:19:27 -0300

    fix syntax

--- src/matrix/column_selector.v
+++ src/matrix/column_selector.v
@@ -5,12 +5,12 @@
 //! so 2 of those columns are exactly the same.
 module column_selector (output [4:0] col, input clock, input reset);
 
-    //               Q     clock    set  reset     D
+    //               Q     clock   set   reset     D
     //             ------- ------ ------ -----  -------
-    flipflop_d1 D4(col[4], clock,  reset  0,     col[0]);
-	flipflop_d  D3(col[3], clock,  0,     reset, col[4]);
-    flipflop_d  D2(col[2], clock,  0,     reset, col[3]);
-    flipflop_d  D1(col[1], clock,  0,     reset, col[2]);
-    flipflop_d  D0(col[0], clock,  0 ,    reset, col[1]);
+    flipflop_d1 D4(col[4], clock, reset, 0,     col[0]);
+	flipflop_d  D3(col[3], clock, 0,     reset, col[4]);
+    flipflop_d  D2(col[2], clock, 0,     reset, col[3]);
+    flipflop_d  D1(col[1], clock, 0,     reset, col[2]);
+    flipflop_d  D0(col[0], clock, 0 ,    reset, col[1]);
 
 endmodule
