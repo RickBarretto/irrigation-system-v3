73213c6caeda989c24e17842587e902672918dfb
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-07-18 15:50:11 -0300

    [source\matrix] revert rows

--- src/matrix/water_supply_decoder.v
+++ src/matrix/water_supply_decoder.v
@@ -8,12 +8,12 @@ module tank_empting_decoder (
     always @(*) begin
         case (water_level)
             3'b000:  column_level <= 7'b1111111;
-            3'b001:  column_level <= 7'b1111110;
-            3'b010:  column_level <= 7'b1111100;
-            3'b011:  column_level <= 7'b1111000;
-            3'b100:  column_level <= 7'b1110000;
-            3'b101:  column_level <= 7'b1100000;
-            3'b110:  column_level <= 7'b1000000;
+            3'b001:  column_level <= 7'b0111111;
+            3'b010:  column_level <= 7'b0011111;
+            3'b011:  column_level <= 7'b0001111;
+            3'b100:  column_level <= 7'b0000111;
+            3'b101:  column_level <= 7'b0000011;
+            3'b110:  column_level <= 7'b0000001;
             3'b111:  column_level <= 7'b0000000;
             default: column_level <= 7'b1111111;
         endcase
